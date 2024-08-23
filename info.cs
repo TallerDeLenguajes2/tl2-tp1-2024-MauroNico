28eaf6dee45610afe7ed0c0610534e399b6c6646
MauroNico <mauronico432@gmail.com>
2024-08-23 19:33:35 -0300

    fifth commit

--- info.cs
+++ info.cs
@@ -36,6 +36,64 @@ public class Pedido
     }
     public string VerDatosCliente(){
         return $"{Cliente.Nombre}, {Cliente.Telefono}, {Cliente.Direccion}";
+        
+    }
+}
+
+class Cadete{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Direccion { get; set; }
+    public string Telefono { get; set; }
+    public List<Pedido> ListadoPedidos { get; set; }
+
+    public Cadete(int id, string nombre, string direccion, string telefono)
+    {
+        Id = id;
+        Nombre = nombre;
+        Direccion = direccion;
+        Telefono = telefono;
+        ListadoPedidos = new List<Pedido>();
+    }
+
+    public void AsignarPedido(Pedido pedido)
+    {
+        ListadoPedidos.Add(pedido);
+    }
+
+    public int JornalACobrar()
+    {
+        int pedidosEntregados = ListadoPedidos.FindAll(p => p.Estado == "Entregado").Count;
+        return pedidosEntregados * 500;
+    }
+}
+
+class Cadeteria{
+    public string Nombre { get; set; }
+    public string Telefono { get; set; }
+    public List<Cadete> ListadoCadetes { get; set; }
+
+    public Cadeteria(string nombre, string telefono)
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+        ListaCadetes = new List<Cadete>();
+    }
+
+    public void CargarCSV(string archivoCSV)
+    {
+        using (var reader = new StreamReader(archivoCSV))
+        {
+            string headerLine = reader.ReadLine(); // Leer la cabecera
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                Cadete cadete = new Cadete(int.Parse(values[0]), values[1], values[2], values[3]);
+                ListaCadetes.Add(cadete);
+            }
+        }
     }
 }
 
