9da37102220f683012d3219a02890e80e39b9ad9
MauroNico <mauronico432@gmail.com>
2024-08-30 20:22:06 -0300

    eighth commit

--- program.cs
+++ program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿class Program{
+    static void main(){
+
+        Cadeteria cadeteria = new Cadeteria("Cadeteria 423", "28937829");
+        cadeteria.CargarCSV("cadetes.csv");
+
+        List<Pedido> pedidos = new List<Pedido>();
+        
+    }
+}
